f2ff265276e459642845e45634c92e4259757a7a
autodocs <autodocs>
2018-06-09 12:47:32 +0000

    build based on bc7da8a

---
+++ latest/search_index.js
@@ -0,0 +1,67 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "TimeseriesSurrogates.jl",
+    "title": "TimeseriesSurrogates.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#TimeseriesSurrogates.jl-1",
+    "page": "TimeseriesSurrogates.jl",
+    "title": "TimeseriesSurrogates.jl",
+    "category": "section",
+    "text": "A Julia package for generating surrogate time series."
+},
+
+{
+    "location": "index.html#Visualizing-surrogate-realizations-1",
+    "page": "TimeseriesSurrogates.jl",
+    "title": "Visualizing surrogate realizations",
+    "category": "section",
+    "text": "Pages = [\n    \"plots/visualize_surrogate.md\"\n    ]\nDepth = 2"
+},
+
+{
+    "location": "index.html#Surrogate-methods-1",
+    "page": "TimeseriesSurrogates.jl",
+    "title": "Surrogate methods",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Random-shuffle-surrogates-1",
+    "page": "TimeseriesSurrogates.jl",
+    "title": "Random shuffle surrogates",
+    "category": "section",
+    "text": "Pages = [\n    \"constrained/randomshuffle/randomshuffle.md\"\n    ]\nDepth = 2"
+},
+
+{
+    "location": "index.html#Fourier-surrogates-1",
+    "page": "TimeseriesSurrogates.jl",
+    "title": "Fourier surrogates",
+    "category": "section",
+    "text": "Pages = [\n    \"constrained/fourier/fourier_surrogates.md\"\n    ]\nDepth = 2"
+},
+
+{
+    "location": "index.html#Amplitude-adjusted-Fourier-surrogates-1",
+    "page": "TimeseriesSurrogates.jl",
+    "title": "Amplitude-adjusted Fourier surrogates",
+    "category": "section",
+    "text": "Pages = [\n    \"constrained/amplitude_adjusted/amplitude_adjusted.md\"\n    ]\nDepth = 2"
+},
+
+{
+    "location": "index.html#All-functions-1",
+    "page": "TimeseriesSurrogates.jl",
+    "title": "All functions",
+    "category": "section",
+    "text": ""
+},
+
+]}

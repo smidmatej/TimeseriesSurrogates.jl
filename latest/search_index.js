e4e29ffa82c905c7adb64e173aa1ecd16b8ed13a
autodocs <autodocs>
2018-07-23 12:26:10 +0000

    build based on 9353f61

--- latest/search_index.js
+++ latest/search_index.js
@@ -61,7 +61,7 @@ var documenterSearchIndex = {"docs": [
     "page": "What is a surrogate?",
     "title": "The method of surrogate testing",
     "category": "section",
-    "text": "The method of surrogate testing [1] is a statistical method for testing properties about a dynamical system whose governing equations is not known. This happens when the only information we have available about the system is a time series.Surrogate testing can be used to test, for example, the following properties about a data set or the underlying process [2]:is the dataset the result of a nongaussian process?\ndoes the data represent chaos?"
+    "text": "The method of surrogate testing [1] is a statistical method for testing properties about a dynamical system whose governing equations is not known. This happens when the only information we have available about the system is a time series.Surrogate testing can be used to test, for example, the following properties about a data set or the underlying process [2]:does the dataset show evidence of nonlinearity?\ndoes the dataset show evidence of low-dimensional chaos?"
 },
 
 {
@@ -85,7 +85,7 @@ var documenterSearchIndex = {"docs": [
     "page": "What is a surrogate?",
     "title": "Constrained realizations",
     "category": "section",
-    "text": "The values of a constrained surrogate realization of a time series is just a shuffling of the values of the original time series.TimeseriesSurrogates.jl provides function to generate the following surrogate realizations.Random shuffle surrogates (RS). Destroys any linear correlation in the signal, but preserves its amplitude distribution.\nFourier surrogates (FS). Have the same linear correlation, or periodogram, as X.\nAmplitude adjusted Fourier transform surrogates. Have the same linear correlation and the amplitude distribution as X."
+    "text": "The values of a constrained surrogate realization of a time series is just a shuffling of the values of the original time series. With TimeseriesSurrogates.jl, you can construct the following types of constrained surrogate realizations:Random shuffle surrogates (RS). Destroys any linear correlation in the signal, but preserves its amplitude distribution.\nFourier surrogates (FS). Have the same linear correlation, or periodogram, as X.\nAmplitude adjusted Fourier transform surrogates. Have the same linear correlation and the amplitude distribution as X."
 },
 
 {
@@ -101,7 +101,7 @@ var documenterSearchIndex = {"docs": [
     "page": "What is a surrogate?",
     "title": "Literature references",
     "category": "section",
-    "text": "[1] Theiler et al., Testing for nonlinearity in time series: the method of surrogate data, Physica D 58 (1992) 77-94. [2] Theiler & Prichard, Constrained-realization Monte-Carlo method for hypothesis testing, Physica D 94 (1996) 221-235"
+    "text": "Theiler et al., Testing for nonlinearity in time series: the method of surrogate data, Physica D 58 (1992) 77-94.\nTheiler & Prichard, Constrained-realization Monte-Carlo method for hypothesis testing, Physica D 94 (1996) 221-235"
 },
 
 {
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Random shuffle (RS)",
     "title": "TimeseriesSurrogates.randomshuffle",
     "category": "function",
-    "text": "randomshuffle(ts)\n\nGenerate a random constrained surrogate for ts.\n\n\n\n"
+    "text": "randomshuffle(ts)\n\nGenerate a random constrained surrogate for ts. Destroys any linear correlation in the signal, but preserves its amplitude distribution.\n\nts Is the time series for which to generate an AAFT surrogate realization.\n\n\n\n"
 },
 
 {
@@ -125,15 +125,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Random shuffle (RS)",
     "title": "Random shuffle surrogates (RS)",
     "category": "section",
-    "text": "The easiest way of constructing a constrained surrogate is just shuffling the time indices of the original time series.TimeseriesSurrogates.randomshuffle"
-},
-
-{
-    "location": "constrained/randomshuffle.html#Example-of-surrogate-generation-by-shuffling-time-indices-1",
-    "page": "Random shuffle (RS)",
-    "title": "Example of surrogate generation by shuffling time indices",
-    "category": "section",
-    "text": "using TimeseriesSurrogates\nts = AR1()\nsurrogate = randomshuffle(ts)\n\nsurrplot(ts, surrogate) # Visualize the surrogate"
+    "text": "TimeseriesSurrogates.randomshuffleThe easiest way of constructing a constrained surrogate is just shuffling the time indices of the original time series. Here\'s an example:using TimeseriesSurrogates\n\n# Generate a time series. Here, we\'ll use an AR1 process.\nts = AR1()\n\n# Generate a random shuffle surrogate realization\nsurrogate = randomshuffle(ts)\n\n# Plot the surrogate along with the time series it is based on, along with autocorrelation\n# and periodogram plots.\nsurrplot(ts, surrogate)"
 },
 
 {
@@ -157,7 +149,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Fourier transform (FT)",
     "title": "TimeseriesSurrogates.randomphases",
     "category": "function",
-    "text": "randomphases(ts)\n\nCreate a random phases surrogate for ts.\n\nFrom J. Theiler et al., Physica D 58 (1992) 77-94 (1992).\n\n\n\n"
+    "text": "randomphases(ts)\n\nCreate a random phases surrogate for ts [1]. Surrogate realizations using the phase surrogates have the same linear correlation, or periodogram, as the original data.\n\nts Is the time series for which to generate an AAFT surrogate realization.\n\nLiterature references\n\nJ. Theiler et al., Physica D 58 (1992) 77-94 (1992).\n\n\n\n"
 },
 
 {
@@ -173,7 +165,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Fourier transform (FT)",
     "title": "How do random phase surrogates vary across realizations?",
     "category": "section",
-    "text": "Below is a plot showing different phase surrogate realizations (orange) over the same time series (blue). Each frame in the animation is a different surrogate realization. Note that the autocorrelations are roughly similar across realizations.randomphases_NSAR2_gif(n_iters = 30, fps = 5)(Image: 30 random phase surrogates for a single realization of a cyclostationary AR(2) process)In the next animation, the time series (blue) for which we generate surrogates change between frames. One phase surrogate realization (orange) is generated for each of the time series. Notice that, again, the phase surrogates accurately represents the autocorrelation of the different initial time series.randomphases_NSAR2_gif(n_iters = 30, fps = 5,\n                        new_realization_every_iter = true)(Image: 30 realizations of a cyclostationary AR(2) process. One random phase surrogate per realization of the time series.)"
+    "text": "Below is a plot showing different phase surrogate realizations (orange) over the same time series (blue). Each frame in the animation is a different surrogate realization. Note that the autocorrelations are roughly similar across realizations.randomphases_NSAR2_gif(n_iters = 30, fps = 5)(Image: 30 random phase surrogates for a single realization of a cyclostationary AR(2) process)In the next animation, the time series (blue) for which we generate surrogates change between frames. One phase surrogate realization (orange) is generated for each of the time series. Notice that, again, the phase surrogates accurately represents the autocorrelation of the different initial time series.randomphases_NSAR2_gif(n_iters = 30, fps = 5, new_realization_every_iter = true)(Image: 30 realizations of a cyclostationary AR(2) process. One random phase surrogate per realization of the time series.)"
 },
 
 {
@@ -181,7 +173,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Fourier transform (FT)",
     "title": "TimeseriesSurrogates.randomamplitudes",
     "category": "function",
-    "text": "randomamplitudes(ts)\n\nCreate a random amplitude surrogate for ts.\n\nA modification of the random phases surrogates (from J. Theiler et al., Physica D 58 (1992) 77-94 (1992)) where amplitudes are adjusted instead.\n\n\n\n"
+    "text": "randomamplitudes(ts)\n\nCreate a random amplitude surrogate for ts.\n\nA modification of the random phases surrogates [1] where amplitudes are adjusted instead of the phases after taking the Fourier transform.\n\nts Is the time series for which to generate an AAFT surrogate realization.\n\nLiterature references\n\nJ. Theiler et al., Physica D 58 (1992) 77-94 (1992))\n\n\n\n"
 },
 
 {
@@ -189,7 +181,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Fourier transform (FT)",
     "title": "Random amplitude surrogates",
     "category": "section",
-    "text": "TimeseriesSurrogates.randomamplitudesThis is how you would generate a random amplitude surrogate realization of a time series.# Generate a time series. Again, we\'ll use a NSAR2() process.\nts = NSAR2()\n\n# Generate a random amplitude surrogate realization\nsurrogate = randomamplitudes(ts)\n\n# Plot the surrogate along with the time series it is based on, along with autocorrelation\n# and periodogram plots.\nsurrplot(ts, surrogate)"
+    "text": "TimeseriesSurrogates.randomamplitudesThis is how you would generate a random amplitude surrogate realization of a time series.using TimeseriesSurrogates\n\n# Generate a time series. Again, we\'ll use a NSAR2() process.\nts = NSAR2()\n\n# Generate a random amplitude surrogate realization\nsurrogate = randomamplitudes(ts)\n\n# Plot the surrogate along with the time series it is based on, along with autocorrelation\n# and periodogram plots.\nsurrplot(ts, surrogate)"
 },
 
 {
@@ -221,7 +213,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Amplitude adjusted Fourier (AAFT)",
     "title": "TimeseriesSurrogates.aaft",
     "category": "function",
-    "text": "aaft(ts)\n\nGenerate a realization of an amplitude adjusted Fourier transform (AAFT) surrogate.\n\nts Is the time series for which to generate an AAFT surrogate realization.\n\nLiterature references\n\nJ. Theiler et al., Physica D 58 (1992) 77-94 (1992).\n\n\n\n"
+    "text": "aaft(ts)\n\nGenerate a realization of an amplitude adjusted Fourier transform (AAFT) surrogate [1].\n\nts Is the time series for which to generate an AAFT surrogate realization.\n\nLiterature references\n\nJ. Theiler et al., Physica D 58 (1992) 77-94 (1992).\n\n\n\n"
 },
 
 {
@@ -245,7 +237,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Amplitude adjusted Fourier (AAFT)",
     "title": "TimeseriesSurrogates.iaaft",
     "category": "function",
-    "text": "Generate an iteratively adjusted amplitude adjusted Fourier transform (IAAFT) surrogate series for ts.\n\nFrom T. Schreiber; A. Schmitz (1996). \"Improved Surrogate Data for Nonlinearity Tests\". Phys. Rev. Lett. 77 (4): 635–638. doi:10.1103/PhysRevLett.77.635. PMID\n\n\n\n\n\n"
+    "text": "iaaft(ts; [n_maxiter = 200, tol = 1e-6, n_window = 50 ])\n\nGenerate an iteratively adjusted amplitude adjusted Fourier transform (IAAFT) [1] surrogate realization of ts.\n\nKeywords\n\nts is the time series for which to generate an AAFT surrogate realization.\n\nn_maxiter sets the maximum number of iterations to allow before ending the algorithm (if convergence is slow).\n\ntol is the relative tolerance for deciding if convergence is achieved.\n\nn_window is the number is windows used when binning the periodogram (used for determining convergence).\n\nLiterature references\n\nT. Schreiber; A. Schmitz (1996). \"Improved Surrogate Data for Nonlinearity Tests\". Phys. Rev. Lett. 77 (4): 635–638. doi:10.1103/PhysRevLett.77.635. PMID 10062864.\n\n\n\n"
 },
 
 {
@@ -253,7 +245,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Amplitude adjusted Fourier (AAFT)",
     "title": "Iterated AAFT (AAFT)",
     "category": "section",
-    "text": "The IAAFT surrogates add an iterative step to the AAFT algorithm improve convergence.TimeseriesSurrogates.iaaft"
+    "text": "TimeseriesSurrogates.iaaftThe IAAFT surrogates add an iterative step to the AAFT algorithm improve convergence."
 },
 
 {
@@ -454,6 +446,62 @@ var documenterSearchIndex = {"docs": [
     "title": "Plot/animation/gif docs",
     "category": "section",
     "text": "You can use any of the following functions to generate plots, animations and the animations as gif files.TimeseriesSurrogates.randomshuffle_plot\nTimeseriesSurrogates.randomamplitudes_plot\nTimeseriesSurrogates.randomphases_plot\nTimeseriesSurrogates.aaft_plot\nTimeseriesSurrogates.iaaft_plot\n\nTimeseriesSurrogates.randomshuffle_anim\nTimeseriesSurrogates.randomamplitudes_anim\nTimeseriesSurrogates.randomphases_anim\nTimeseriesSurrogates.aaft_anim\nTimeseriesSurrogates.iaaft_anim\n\nTimeseriesSurrogates.randomshuffle_gif\nTimeseriesSurrogates.randomamplitudes_gif\nTimeseriesSurrogates.randomphases_gif\nTimeseriesSurrogates.aaft_gif\nTimeseriesSurrogates.iaaft_gif"
+},
+
+{
+    "location": "man/exampleprocesses.html#",
+    "page": "Example systems",
+    "title": "Example systems",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "man/exampleprocesses.html#TimeseriesSurrogates.NLNS",
+    "page": "Example systems",
+    "title": "TimeseriesSurrogates.NLNS",
+    "category": "function",
+    "text": "NLNS(n_steps, x₀)\n\nA nonlinear, nonstationary process given by the following stochastic model [1]:\n\nx_t = x_t-1 + a_t a_t-1\n\nwith a_t  N(01) being stationary. The nonlinearity lies in the a_t a_t-1 term. This is, in essence, a nonlinear random walk. x₀ sets the initial condition.\n\nLiterature references\n\nLucio et al., Phys. Rev. E 85, 056202 (2012).\n\n\n\n"
+},
+
+{
+    "location": "man/exampleprocesses.html#TimeseriesSurrogates.SNLST",
+    "page": "Example systems",
+    "title": "TimeseriesSurrogates.SNLST",
+    "category": "function",
+    "text": "SNLST(n_steps, x₀, k)\n\nDynamically linear process transformed by a strongly nonlinear static transformation (SNLST), given by the following map [1]:\n\nx(t) = k*x(t-1) + a(t)\n\nwith the transformation s(t) = x(t)^3.\n\nLiterature references\n\nLucio et al., Phys. Rev. E 85, 056202 (2012).\n\n\n\n"
+},
+
+{
+    "location": "man/exampleprocesses.html#TimeseriesSurrogates.randomwalk",
+    "page": "Example systems",
+    "title": "TimeseriesSurrogates.randomwalk",
+    "category": "function",
+    "text": "randomwalk(n_steps, x₀)\n\nLinear random walk (AR(1) process with a unit root) [1]\n\nThis is an example of a nonstationary linear process.\n\nLiterature references\n\nLucio et al., Phys. Rev. E 85, 056202 (2012).\n\n\n\n"
+},
+
+{
+    "location": "man/exampleprocesses.html#TimeseriesSurrogates.NSAR2",
+    "page": "Example systems",
+    "title": "TimeseriesSurrogates.NSAR2",
+    "category": "function",
+    "text": "NSAR2(n_steps, x₀, x₁)\n\nCyclostationary AR(2) process given by the following map [2]\n\nx_t = a(t) * x_t-1 + a x_t-2 + _t.\n\nwhere\n\na(t) = 2 cos2pi  T(t) * exp(-1  ),\n\nT(t)  = T_0 + M * sin(2 pi t  T_mod),\n\na    = exp(-2  	au),\n\nand x₀ and x₁ sets the initial conditions.\n\nLiterature references\n\nLucio et al., Phys. Rev. E 85, 056202 (2012), after J. Timmer, Phys. Rev. E 58, 5153\n\n(1998).\n\n\n\n"
+},
+
+{
+    "location": "man/exampleprocesses.html#TimeseriesSurrogates.AR1",
+    "page": "Example systems",
+    "title": "TimeseriesSurrogates.AR1",
+    "category": "function",
+    "text": "AR1(n_steps, x₀, k)\n\nSimple AR(1) model with no static transformation given by the following map [1]:\n\nxi = k*xi-1 + ai,\n\nwhere a[i] is a draw from a normal distribution with zero mean and unit variance. x₀ sets the initial condition and k is the tunable parameter in the map.\n\nLiterature references\n\nLucio et al., Phys. Rev. E 85, 056202 (2012).\n\n\n\n"
+},
+
+{
+    "location": "man/exampleprocesses.html#Example-processes-1",
+    "page": "Example systems",
+    "title": "Example processes",
+    "category": "section",
+    "text": "NLNS\nSNLST\nrandomwalk\nNSAR2\nAR1"
 },
 
 ]}
